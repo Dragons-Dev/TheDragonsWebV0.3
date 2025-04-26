42f39332f25aba97f2cace35be35505f048789da
DTheDragonFire <dthedragonfire@thedragons.xyz>
2025-04-26 16:19:13 +0200

    feat: init git && base - rest

---
+++ WebsiteV3/src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import "./assets/base.css"
+</script>
+
+<template>
+  <html>
+    <header>
+      <a href="/">
+      <img src="./components/icons/DiscordServerEmoji-Rund.png" alt="DiscordIcon">
+      <h1>DragonsWeb</h1>
+      </a>
+    </header>
+  <RouterView />
+    <footer>
+      <ul>
+        <li>Copyright</li>
+        <li><RouterLink to="/about">About</RouterLink></li>
+      </ul>
+    </footer>
+  </html>
+
+</template>
+
+<style scoped>
+
+</style>
