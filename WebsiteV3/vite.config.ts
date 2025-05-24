6af2d5decbaf75c4ec75613f452a4a719f025d55
DTheIcyDragon <dtheicydragon@thedragons.xyz>
2025-05-25 00:53:35 +0200

    fix: remove requirement for ssl certificate

--- WebsiteV3/vite.config.ts
+++ WebsiteV3/vite.config.ts
@@ -3,17 +3,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import * as fs from "node:fs"
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     host: true,
     cors: true,
-    https: {
-      key: fs.readFileSync("du-key.pem"),
-      cert: fs.readFileSync("du.pem")
-    },
   },
   plugins: [
     vue(),
