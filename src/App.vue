fdf404f1d964832fc0f144474dd4cf4afb072dc4
DTheDragonFire <dthedragonfire@thedragons.xyz>
2025-06-01 22:51:49 +0200

    sync: login Box

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,8 @@ import {
   logCookie,
   checkCookie
 } from "./scripts/Cookies.ts"
+import loginBox from "./components/templates/loginBox.vue"
+const boxLogin = ref(false)
 </script>
 
 <script lang="ts">
@@ -28,13 +30,21 @@ import {
       <img src="./components/icons/DiscordServerEmoji-Rund.png" alt="DiscordIcon">
       <h1>DragonsWeb</h1>
       </a>
-      <ul>
-        <li v-if="loggedIn"><button>{{ username }}</button></li>
-        <li v-else><button>Anmelden</button></li>
+      <ul class="login-buttons">
+        <li v-if="loggedIn"><button @click="" >{{ username }}</button></li>
+        <li v-else><button @click="boxLogin = !boxLogin">Anmelden</button></li>
         <li v-if="loggedIn"><button>Abmelden</button></li>
         <li v-else><button>Konto erstellen</button></li>
       </ul>
+
+      <transition mode="out-in">
+        <!--TODO animation wie bei W3Schools mit dem von Paul -->
+        <loginBox id="login" v-if="boxLogin"></loginBox>
+        <!--TODO einfügen von register und konto als vorschau -->
+      </transition>
     </header>
+
+
 
 
   <RouterView />
---
+++ src/components/templates/loginBox.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div id="" class="loginBox">
+    <label for="mail">Deine E-Mail:</label><br>
+    <input id="mail" type="email" placeholder="Deine E-Mail" size="40" required><br>
+    <hr>
+    <label for="passw">Dein Passwort:</label><br>
+    <input id="passw" type="password" placeholder="Password" size="40" required><br>
+    <hr>
+    <input id="stayLog" type="checkbox"/>
+    <label for="stayLog">Angemeldet bleiben</label><br>
+    <hr>
+    <button>Anmelden</button>
+  </div>
+</template>
+
+<style scoped>
+.loginBox {
+  background-color: #295e85;
+  border: 5px solid #10283a;
+  border-radius: 10px;
+  padding: 0.5em;
+  position: absolute;
+  right: 0.5em;
+  top: 5em;
+  z-index: 1;
+}
+
+.loginBox > label {
+  font-size: 1em;
+  font-weight: bold;
+  color: white;
+}
+
+.loginBox > input {
+  border-radius: 0.5em;
+  border: 3px solid #10283a;
+  padding: 0.25em;
+}
+
+.loginBox > hr {
+  border: 2px solid #10283a;
+}
+
+.loginBox > button {
+  position: relative;
+  left: 37%;
+  color: white;
+  font-size: 1em;
+  background-color: #23374b;
+  padding: 0.25em;
+  border: 2px solid black;
+  border-radius: 5px;
+  transition: 500ms;
+}
+
+.loginBox > button:hover {
+  background-color: #295e85;
+  border-radius: 3px;
+  border-color: #10283a;
+}
+</style>
